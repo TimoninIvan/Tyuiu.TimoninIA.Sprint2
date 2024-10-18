411c7706c4a8c52db63b0fe2dc972c401b205b52
Ivan Timonin <Tyuiu.TimoninIA@outlook.com>
2024-10-18 23:28:42 +0600

    Task3

---
+++ Tyuiu.TimoninIA.Sprint2.Task3.Lib/DataService.cs
@@ -0,0 +1,74 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+namespace Tyuiu.TimoninIA.Sprint2.Task3.V22.Lib
+
+{
+    public class DataService : ISprint2Task3V22
+    {
+        public double Calculate(double x)
+        {
+            double y = 0;
+
+
+            if (x > 1)
+            {
+                y = x + Math.Pow(((x + 3) / (x - 1)), x);
+            }
+            else
+            {
+                if (x == 0)
+                {
+                    y = (Math.Pow(x, 2) - Math.Pow(Math.Cos(x), 2) + 10) / (Math.Pow(x, 2) - Math.Pow(Math.Sin(x), 2) + 12);
+                }
+                else
+                {
+                    if ((x > -26) && (x < 2))
+                    {
+                        y = Math.Pow(3 + 2 / Math.Pow(x, 2), x);
+                    }
+                    else
+                    {
+                        if (x < -26)
+                        {
+                            y = x + 10 * x - (1 / x);
+                        }
+                    }
+                }
+            }
+            return Math.Round(y, 3);
+        }
+    }
+}
+
+//public double Calculate(double x)
+//{
+//double y = 0;
+
+
+//if (x > 0)
+//{
+//    y = x + 12;
+//}
+//else
+//{
+//    if (x == 0)
+//    {
+//        y = 1 / (x + 3);
+//    }
+//    else
+//    {
+//        if ((x > -3) && (x < 0))
+//        {
+//            y = Math.Sin(x);
+//        }
+//        else
+//        {
+//            if (x < -3)
+//            {
+//                y = ((x - 5) / (x + 3)) + x;
+//            }
+//        }
+//    }
+//}
+//return Math.Round(y, 3);
---
+++ Tyuiu.TimoninIA.Sprint2.Task3.V22/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.TimoninIA.Sprint2.Task3.V22.Lib;
+
+namespace Tyuiu.TimoninIA.Sprint2.Task3.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                   *");
+            Console.WriteLine("**************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                         *");
+            Console.WriteLine("**************************************************************************************");
+
+            Console.WriteLine("Введите значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
